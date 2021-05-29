44647730a24581b407ad4145b75c84f8aa874364
Robertosnap <robertosnap@pm.me>
2021-05-29 12:24:21 +0200

    Creating mapping

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,33 +1,46 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   CapTableRegistry,
   capTableAdded,
-  capTableRemoved
-} from "../generated/CapTableRegistry/CapTableRegistry"
-import { ExampleEntity } from "../generated/schema"
+  capTableRemoved,
+} from "../generated/CapTableRegistry/CapTableRegistry";
+import { ERC1400 } from "../generated/CapTableRegistry/ERC1400";
+import { CapTableRegistry as CapTableRegistrySchema } from "../generated/schema";
+import { CapTable as CapTableSchema } from "../generated/schema";
 
 export function handlecapTableAdded(event: capTableAdded): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let capTableRegistry = CapTableRegistrySchema.load(event.address.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (capTableRegistry == null) {
+    capTableRegistry = new CapTableRegistrySchema(event.address.toHex());
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    capTableRegistry.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  capTableRegistry.count = capTableRegistry.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
-  entity.capTableAddress = event.params.capTableAddress
+  let capTable = new CapTableSchema(event.params.capTableAddress.toHex());
+  let erc1400Contract = ERC1400.bind(event.params.capTableAddress);
+  let capTableRegistryContract = CapTableRegistry.bind(event.address);
+  let info = capTableRegistryContract.info(event.params.capTableAddress);
+
+  capTable.active = true;
+  capTable.orgnr = info.value0.toString();
+  capTable.minter = erc1400Contract.owner();
+  capTable.registry = capTableRegistry.id;
+  capTable.totalSupply = erc1400Contract.totalSupply();
+  capTable.controllers = erc1400Contract.controllers();
+  capTable.tokenBalances = [];
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  capTableRegistry.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
